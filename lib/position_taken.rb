b5ce5e9f02917775099ea15911f57d95801beb42
generic-puppet-7755 <tasmith1990@gmail.com>
2019-10-22 20:41:54 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,29 @@
 # code your #position_taken? method here!
+
+def position_taken?(board, index)
+  
+  if board.at(index) == "X" or board.at(index) == "O"
+    return true
+  
+  elsif !!board.at(index)
+    return false
+  
+  elsif board.at(index) == nil
+    return false
+  end
+  
+end
+
+board = Array.new(9, " ")
+index = 0
+
+position_taken?(board, index)
+
+board[0] = nil
+position_taken?(board, index)
+
+board[0] = "X"
+position_taken?(board, index)
+
+board[0] = "O"
+position_taken?(board, index)
